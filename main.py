99633177fefa0d29c54614d045be2592ba08c1dd
sarbosarcar <sarbosarcar@gmail.com>
2025-01-09 00:20:54 +0530

    Update index.html and main.py

--- main.py
+++ main.py
@@ -29,29 +29,44 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/{cliplink}", response_class=HTMLResponse)
 async def read_item(cliplink: str, request: Request):
     response = supabase.table("clipboard").select("encoded").eq("link", cliplink).execute().data
+    images = supabase.table("clipboard").select("images").eq("link", cliplink).execute().data
+    if images:
+        img = images[0]["images"]
+    else:
+        img = []
     if response == []:
         return templates.TemplateResponse(
-            request=request, name="index.html", context={"link": cliplink, "prefill": ""}
+            request=request, name="index.html", context={"link": cliplink, "prefill": "", "images": img}
         )
     else:
         prefill = response[0]["encoded"]
         prefill = base64.b64decode(prefill.encode("utf-8")).decode("utf-8")
         return templates.TemplateResponse(
-            request=request, name="index.html", context={"link": cliplink, "prefill": prefill}
+            request=request, name="index.html", context={"link": cliplink, "prefill": prefill, "images": img}
         )
 
 @app.post("/{cliplink}/save")
-async def write_item(request: Request, cliplink: str, content: str = Form(...)):
+async def write_item(request: Request, cliplink: str, content: str = Form(...), img_content: str = Form(...)):
     string = content.encode("utf-8")
     base64_enc = base64.b64encode(string)
     base64_string = base64_enc.decode("utf-8")
     response = supabase.table("clipboard").select("link").eq("link", cliplink).execute().data
+    imgs = img_content.split("\n")
+    imgs_ret = []
+    for img in imgs:
+        if img.strip("\r"):
+            imgs_ret.append(img.strip("\r"))
+    
     if response == []:
         if content != "":
             supabase.table("clipboard").insert({"link": cliplink, "encoded": base64_string}).execute()
+        if imgs_ret:
+            supabase.table("clipboard").insert({"images": imgs_ret}).execute()
     else:
         if content != "":
             supabase.table("clipboard").update({"encoded": base64_string}).eq("link", cliplink).execute()
+        if imgs_ret:
+            supabase.table("clipboard").update({"images": imgs_ret}).eq("link", cliplink).execute()
 
     return RedirectResponse(f"/{cliplink}", status_code=status.HTTP_303_SEE_OTHER)
 
